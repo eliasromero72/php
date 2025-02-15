faf74a328301b7e59eb33d4e0557397456ba4388
Elias Romero <102391069+eliasromero72@users.noreply.github.com>
2025-02-14 22:06:20 -0300

    Add files via upload

---
+++ intro.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+//declarando variáveis
+$x = 5; //$x é um número inteiro
+$y = 6.0; //$y é um número real
+$nome = "Elias Romero"; //$nome é uma string 
+
+echo $x . "<br>";
+echo "Eu gosto do prof. " . $nome . "!" . "<br>";
+
+var_dump($x);
+var_dump($y);
+var_dump($nome);
+
+echo "<br>";
+
+echo $x + $y;
+
+?>
+
+</body>
+</html>
---
+++ olaMundo.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+/*
+echo "O Palmeiras tem Mondial!" . "<br>";
+echo "Versão do PHP: " . phpversion();
+*/
+
+?>
+
+</body>
+</html>
